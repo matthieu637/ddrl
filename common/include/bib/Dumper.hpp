6049cce3e170d8855bc4691cdedce242783a52d9
Matthieu Zimmer <zimme0511@etumail.uhp-nancy.fr>
2015-02-21 15:54:09 +0100

    Dumper to call operator<< with multiple args

--- common/include/bib/Dumper.hpp
+++ common/include/bib/Dumper.hpp
@@ -1,8 +1,44 @@
 #ifndef DUMPER_H
 #define DUMPER_H
 
+#include <tuple>
+
+namespace helper
+{
+    template <int... Is>
+    struct index {};
+
+    template <int N, int... Is>
+    struct gen_seq : gen_seq<N - 1, N - 1, Is...> {};
+
+    template <int... Is>
+    struct gen_seq<0, Is...> : index<Is...> {};
+}
+
+namespace bib {
+
+template<typename Displayable, typename... Args>
 class Dumper
 {
+public:
+    Dumper(Displayable* _ptr, Args... _args) : ptr(_ptr), params(_args...)
+    {
+    }
+    
+    template <int... Is>
+    std::ostream& deploy(std::ostream& out, helper::index<Is...>) const{
+	return ptr->display(out, std::get<Is>(params)...);
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Dumper &disp){    
+	return disp.deploy(out, helper::gen_seq<sizeof...(Args)>{});
+    }
+    
+private:
+    Displayable* ptr;
+    std::tuple<Args...> params;
 };
+
+}
 
 #endif // DUMPER_H
