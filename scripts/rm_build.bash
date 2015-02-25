7f473febd3668153a46c128b40d22ba24e79f0ac
Matthieu Zimmer <contact@matthieu-zimmer.net>
2015-02-25 17:06:34 +0100

    function to remove the builded libraries

--- scripts/rm_build.bash
+++ scripts/rm_build.bash
@@ -5,7 +5,16 @@ cd $LIB
 
 . locate.bash
 
+function rm_build_lib(){
+        goto_root
+        if [ `find . -name 'lib' -type d | wc -l` -ne 0 ] ; then
+                find . -name 'lib' -type d | xargs rm -r  
+        fi 
+}
+
 function rm_build(){
+	rm_build_lib
+
 	goto_root
 	if [ `find . -name 'build' -type d | wc -l` -ne 0 ] ; then
 		find . -name 'build' -type d | xargs rm -r 
