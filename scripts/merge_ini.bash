82c2d1399d084bbad8404ff9df8b0177a92edf1e
Matthieu Zimmer <contact@matthieu-zimmer.net>
2015-03-02 16:33:07 +0100

    improve merge .ini algorithm

--- scripts/merge_ini.bash
+++ scripts/merge_ini.bash
@@ -20,17 +20,99 @@ if [ ! -e $injector ] ; then
 fi
 
 tmp=`mktemp`
-diff -DVERSION1 $base $injector > $tmp
-grep -v '^#if' $tmp | grep -v '^#endif' | grep -v '#else' > $destination
-mv $destination $tmp
 
-duplicates="`cat $tmp | grep '=' | grep -v -e '^#' | cut -d'=' -f1 | sort | uniq -d`"
+#
+# Read the base file and copy it to $tmp by remplacing the duplicate
+#
+while read line
+do
+	isComment=`echo "$line" | grep -e '^#' | wc -l`
+	isSection=`echo "$line" | grep -e '^[[]' | wc -l`
+	isBlank=`echo "$line" | grep -v -e '^#' | grep -v -e '^[[]' | grep -v '[=]' | wc -l`
 
-echo "$duplicates" | while read dupl ; do
-	sed -i "0,/\($dupl\)/s//#\1/" $tmp
-	#dupl=`echo $dupl | sed -e 's/[_]/[_]/g'`
-	#sed -i "s/$dupl//" $tmp
-	#echo "$dupl"
-done
+	if [[ $isComment -eq 1 || $isSection -eq 1 || $isBlank -eq 1 ]] ; then
+		echo "$line" >> $tmp
+	else
+		key=`echo "$line" | sed -e 's/[_]/\\\_/g' | cut -d'=' -f1`
+		isDuplicate=`grep -i "^$key" $injector | wc -l`
+
+		if [ $isDuplicate -eq 1 ]; then
+			newL=`grep -i "$key" $injector`
+			echo "$newL" >> $tmp
+		else
+			echo "$line" >> $tmp
+		fi
+	fi
+
+done < $base
+
+#
+# Inject a line before a specific [section]
+#
+# <file> <section> <line>
+function inject_line_before_section(){
+	file=$1
+	section=$2
+	line=$3
+
+	str_sections_base=`cat $base | grep -e '^[[]'`
+	read -a sections_base <<< $str_sections_base
+	
+	i=0;
+	secfinal=""
+	for sec in "${sections_base[@]}"
+	do	
+		if [[ "$sec" == "$section"  ]] ;
+		then
+			i=`expr $i + 1`
+			secfinal="${sections_base[$i]}"
+			break
+		fi
+		i=`expr $i + 1`
+	done
+
+	section="$secfinal"
+
+	isBlankSec=`echo "$section" | grep -v -e '^[[]' | wc -l`
+	if [[ $isBlankSec -eq 1 ]]; then
+		echo "$line" >> $file
+	else
+		#escape [section] to \[section\]
+		section=`echo $section | sed -e 's/[[]/\\\[/' | sed -e 's/[]]/\\\]/' `
+		sed -i "s/\($section\)/$line\n\1/" $file
+	fi
+}
+
+#
+# Put the rest (new key=value + comment) from injector to $tmp
+#
+
+currentSection=-1
+str_sections=`cat $injector | grep -e '^[[]'`
+read -a sections <<< $str_sections
+
+while read line
+do
+	isComment=`echo "$line" | grep -e '^#' | wc -l`
+	isSection=`echo "$line" | grep -e '^[[]' | wc -l`
+	isKey=`echo "$line" | grep -v -e '^#' | grep -v -e '^[[]' | grep -e '[=]' | wc -l`
+
+	if [ $isSection -eq 1 ] ; 
+	then
+		currentSection=`expr $currentSection + 1`
+	elif [[ $isComment -eq 1 || $isKey -eq 0 ]] ; 
+	then
+		inject_line_before_section $tmp "${sections[$currentSection]}" "$line"
+	elif [ $isKey -eq 1 ] ; 
+	then
+		key=`echo "$line" | sed -e 's/[_]/\\\_/g' | cut -d'=' -f1`
+		isDuplicate=`grep -i "^$key" $base | wc -l`
+
+		if [ $isDuplicate -eq 0 ] ; then
+			inject_line_before_section $tmp "${sections[$currentSection]}" "$line"
+		fi
+	fi
+
+done < $injector
 
 mv $tmp $destination
