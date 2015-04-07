7d1d9f891b41b7fd12d6f059f6da595668c86028
Matthieu Zimmer <contact@matthieu-zimmer.net>
2015-04-07 11:55:35 +0200

    continuous version of q learning

--- agent/qlearning-nn/include/MLP.hpp
+++ agent/qlearning-nn/include/MLP.hpp
@@ -1,15 +1,46 @@
 #ifndef MLP_H
 #define MLP_H
 
+//OPT++ param
+#define HAVE_NAMESPACES
+#define HAVE_STD
+
+#include <vector>
+#include <functional>
+
 #include "doublefann.h"
+#include "opt++/NLF.h"
+#include "opt++/BoundConstraint.h"
+#include "opt++/LinearInequality.h"
+#include "opt++/CompoundConstraint.h"
+#include "opt++/OptNIPS.h"
+//     OptNewton(&nlp): Newton method for unconstrained problems
+//     OptBCNewton(&nlp): Newton method for bound-constrained problems
+//     OptBaNewton(&nlp): Newton method for bound-constrained problems
+//     OptNIPS(&nlp): nonlinear interior-point method for generally constrained problems
+
+using namespace OPTPP;
+
+using NEWMAT::ColumnVector;
+using NEWMAT::Matrix;
+using NEWMAT::SymmetricMatrix;
+
+typedef struct fann* NN;
+struct passdata{
+    NN neural_net;
+    const std::vector<float>& inputs;
+};
+
+void init_hs65(int ndim, ColumnVector& x);
+void hs65(int mode, int ndim, const ColumnVector& x, double& fx,
+          ColumnVector& gx, SymmetricMatrix& Hx, int& result, void* data);
 
 class MLP
 {
 public:
-    typedef struct fann* NN;
 
-    MLP(unsigned int input, unsigned int hidden, unsigned int output) {
-        neural_net = fann_create_standard(3, input, hidden, output);
+    MLP(unsigned int input, unsigned int hidden, unsigned int sensors, float alpha) : size_input_state(input), size_sensors(sensors), size_motors(size_input_state - sensors) {
+        neural_net = fann_create_standard(3, input, hidden, 1);
 
         fann_set_activation_function_hidden(neural_net, FANN_SIGMOID_SYMMETRIC);
 
@@ -18,11 +49,87 @@ public:
         fann_set_train_error_function(neural_net, FANN_ERRORFUNC_LINEAR);
         fann_set_training_algorithm(neural_net, FANN_TRAIN_INCREMENTAL);
         fann_set_train_stop_function(neural_net, FANN_STOPFUNC_MSE);
-//         fann_set_learning_rate(neural_net, this->param.alpha);
+        fann_set_learning_rate(neural_net, alpha);
     }
+
+    ~MLP() {
+        fann_destroy(neural_net);
+    }
+
+    void learn(const std::vector<float>& sensors, const std::vector<float>& motors, float toval) {
+        fann_type out[1];
+        out[0] = toval;
+        
+        uint m = sensors.size();
+        uint n = motors.size();
+        fann_type* inputs = new fann_type[m + n];
+        for (uint j = 0; j < m ; j++)
+            inputs[j] = sensors[j];
+        for (uint j = m; j < m + n; j++)
+            inputs[j] = motors[j - m];
+
+        fann_train(neural_net, inputs, out);
+        delete inputs;
+    }
+    
+    double computeOut(const std::vector<float>& sensors, const std::vector<float>& motors){
+        uint m = sensors.size();
+        uint n = motors.size();
+        fann_type* inputs = new fann_type[m + n];
+        for (uint j = 0; j < m ; j++)
+            inputs[j] = sensors[j];
+        for (uint j = m; j < m + n; j++)
+            inputs[j] = motors[j - m];
+
+        fann_type* out = fann_run(neural_net, inputs);
+        delete inputs;
+        return out[1];
+    }
+
+    std::vector<float>* optimized(const std::vector<float>& inputs) {
+        std::vector<float>* a = new std::vector<float>(size_motors);
+
+        uint ndim = size_motors;
+        ColumnVector lower(ndim), upper(ndim);
+
+        for (uint i = 0; i < ndim; i++) {
+            lower << -1.0f ;
+            upper <<  1.0f ;
+        }
+
+        Constraint c1 = new BoundConstraint(ndim, lower, upper);
+        CompoundConstraint* constraints = new CompoundConstraint(c1);
+
+        passdata d = {neural_net, inputs};
+        NLF2 nips(ndim, hs65, init_hs65, constraints, &d);
+
+        OptNIPS objfcn(&nips);
+
+// The "0" in the second argument says to create a new file.  A "1"
+// would signify appending to an existing file.
+
+        objfcn.setOutputFile("/dev/null", 1);
+        objfcn.setFcnTol(1.0e-06);
+        objfcn.setMaxIter(20);
+        objfcn.setMeritFcn(ArgaezTapia);
+
+        objfcn.optimize();
+
+        //objfcn.printStatus("Solution from nips");
+        //objfcn.cleanup();
+        
+        for(uint i=0;i<a->size();i++)
+          a->at(i) = objfcn.getXPrev()(i+1);
+
+        return a;
+    }
+
 
 private:
     NN neural_net;
+    unsigned int size_input_state;
+    unsigned int size_sensors;
+    unsigned int size_motors;
 };
 
 #endif // MLP_H
--- agent/qlearning-nn/src/MLP.cpp
+++ agent/qlearning-nn/src/MLP.cpp
@@ -1,1 +1,143 @@
 #include "MLP.hpp"
+
+#include "doublefann.h"
+#include <fann_data.h>
+#include "bib/Assert.hpp"
+
+
+#define activation_function(x, lambda) (2.0f/(1.0f + exp(- lambda * x)) - 1.0f)
+//#define activation_function(x, lambda) (2.0f/(1.0f + exp(- 2 * lambda * x)) - 1.0f)
+
+void init_hs65(int ndim, ColumnVector& x)
+{
+    for (uint i = 1; i <= (uint) ndim; i++)
+        x(i) = 0;
+}
+
+class my_weights
+{
+    typedef struct fann_connection sfn;
+
+public:
+    my_weights(NN neural_net, const std::vector<float>& sensors, const ColumnVector& x, uint _m, uint _n) : m(_m), n(_n) {
+        _lambda = fann_get_activation_steepness(neural_net, 1, 0);
+
+        unsigned int number_connection = fann_get_total_connections(neural_net);
+        connections = reinterpret_cast<sfn*>(calloc(number_connection, sizeof(sfn)));
+
+        fann_get_connection_array(neural_net, connections);
+
+        uint number_layer = fann_get_num_layers(neural_net);
+        layers = reinterpret_cast<uint*>(calloc(number_layer, sizeof(sfn)));
+
+        fann_get_layer_array(neural_net, layers);
+        ASSERT(number_layer == 3, number_layer);
+
+        Ci.clear();
+        Ci.resize(h());
+        for (uint i = 0; i < h(); i++) {
+            Ci[i] = 0;
+            for (uint j = 0; j < m; j++)
+                Ci[i] += sensors[j] * connections[i + h() * j].weight;
+
+            Ci[i] += connections[ i + h() * (m + n)].weight;
+        }
+
+        Di.clear();
+        Di.resize(h());
+        for (uint i = 0; i < h(); i++) {
+            Di[i] = Ci[i];
+            for (uint j = 1; j <= n; j++)
+                Di[i] += x(j) * connections[i + h() * (j - 1 + m)].weight;
+        }
+        
+        ASSERT(number_connection == h()*(m+n+1) + (h() + 1), "h : " << h() << " , m + n + 1 : " << m+n+1 << " , nb con : " << number_connection << " , n :" << n );
+    }
+
+    ~my_weights() {
+        free(connections);
+        free(layers);
+    }
+
+    double v(uint i) {
+        return connections[h() * (m + n + 1) + i].weight;
+    }
+
+    double w(uint j, uint i) {
+        return connections[i + (h() * j)].weight;
+    }
+
+    uint h() {
+        return layers[1];
+    }
+
+    double C(uint i) {
+        return Ci[i];
+    }
+
+    double D(uint i) {
+        return Di[i];
+    }
+
+    double lambda() {
+        return _lambda;
+    }
+
+private :
+    sfn* connections;
+    uint* layers;
+    std::vector<double> Ci;
+    std::vector<double> Di;
+    uint m, n;
+    double _lambda;
+};
+
+void hs65(int mode, int ndim, const ColumnVector& x, double& fx,
+          ColumnVector& gx, SymmetricMatrix& Hx, int& result, void* data)
+{
+    passdata* d = (passdata*) data;
+    NN neural_net = d->neural_net;
+
+    uint n = ndim;
+    uint m = fann_get_num_input(neural_net) - n;
+
+    if (mode & NLPFunction) {
+        fann_type* inputs = new fann_type[m + n];
+        for (uint j = 0; j < m ; j++)
+            inputs[j] = d->inputs[j];
+        for (uint j = m; j < m + n; j++)
+            inputs[j] = x(j - m + 1);
+
+        fann_type* out = fann_run(neural_net, inputs);
+        fx = out[1];
+        result = NLPFunction;
+        delete[] inputs;
+    }
+
+    if (mode & NLPGradient) {
+        my_weights _w(neural_net, d->inputs, x, m, n);
+        for (uint j = 1; j <= n; j++) {
+            gx(j) = 0;
+            for (uint i = 0; i < _w.h() ; i++) {
+                double der = activation_function(_w.D(i), _w.lambda());
+                gx(j) = gx(j) + _w.v(i) * _w.w(m + j - 1, i) * _w.lambda() * (1 - der * der);
+            }
+        }
+        result = NLPGradient;
+    }
+
+    if (mode & NLPHessian) {
+        my_weights _w(neural_net, d->inputs, x, m, n);
+        for (uint j = 1; j <= n; j++) {
+            for (uint k = 1; k <= n; k++) {
+              Hx(j,k) = 0;
+              for (uint i = 0; i < _w.h() ; i++) {
+                  double der = activation_function(_w.D(i), _w.lambda());
+                  double Lambda_ij = _w.v(i) * _w.w(m + j - 1, i) * _w.lambda();
+                  Hx(j, k) = Hx(j, k) + -2.f * _w.lambda() * _w.w(m + k - 1, i) * Lambda_ij * der * (1 - der * der);
+              }
+            }
+        }
+        result = NLPHessian;
+    }
+}
