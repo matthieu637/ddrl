a4fad8eb232690553cf4482ade4f99147073a445
Matthieu Zimmer <contact@matthieu-zimmer.net>
2015-04-09 14:09:36 +0200

    memory check scripts

---
+++ scripts/check_utest_memory.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+LIB=$(dirname "${BASH_SOURCE[0]}")
+cd $LIB
+
+. locate.bash
+
+function run_all_test(){
+	goto_root
+	find . -type f -name 'unit-test' | while read atest ; do
+		cd $(dirname $atest)
+		tmp=`mktemp`
+		valgrind --tool=memcheck --leak-check=full --show-reachable=yes --track-origins=yes --leak-resolution=high ./unit-test >& $tmp
+		if [ $? -ne 0 ] ; then
+			cat $tmp
+			rm $tmp
+			exit 1
+		fi
+		if [ `cat $tmp |& grep ERROR |& grep '0 errors' | wc -l` -ne 1 ] ; then
+			cat $tmp
+			rm $tmp
+			exit 1
+		fi
+		rm $tmp
+		cd -
+	done
+}
+
+
+run_all_test
+
+exit 0
+
