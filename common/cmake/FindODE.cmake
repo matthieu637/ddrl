86299857b9fbb3edcde0ccca2e6c055288ac0edc
Matthieu Zimmer <contact@matthieu-zimmer.net>
2015-04-09 15:57:15 +0200

    fix ode precision to double

--- common/cmake/FindODE.cmake
+++ common/cmake/FindODE.cmake
@@ -8,60 +8,16 @@
 
 
 # To build ODE in realease mode :
-# ./configure --prefix=/usr/local --enable-shared --enable-libccd --enable-double-precision --disable-asserts --program-suffix=-r
-# make
-# make DESTDIR=${pkgdir}/ install
-# 
-# for files in "$pkgdir"/usr/local/lib/libode*; do
-#       mv "$files" "$(echo $files | sed -r 's/libode[.]/libode-r./')" 
-# done
-#
-# or makepkg PKGBUILD
+# makepkg PKGBUILD
 
 
 IF (NOT ODE_FOUND)
-
-#   FIND_PROGRAM(ODE_CONFIG ode-config)
-#   IF(ODE_CONFIG)
-#       # Use the newer EXECUTE_PROCESS command if it is available.
-#       IF(COMMAND EXECUTE_PROCESS)
-#         EXECUTE_PROCESS(
-#           COMMAND ${ODE_CONFIG} --cflags
-#           OUTPUT_VARIABLE ODE_CONFIG_CFLAGS
-#           OUTPUT_STRIP_TRAILING_WHITESPACE
-#           RESULT_VARIABLE ODE_CONFIG_RESULT
-#           )
-#       ELSE(COMMAND EXECUTE_PROCESS)
-#         EXEC_PROGRAM(${ODE_CONFIG} ARGS "--cflags"
-#           OUTPUT_VARIABLE ODE_CONFIG_CFLAGS
-#           RETURN_VALUE ODE_CONFIG_RESULT
-#           )
-#       ENDIF(COMMAND EXECUTE_PROCESS)
-# 
-#       # Parse the include flags.
-#       IF("${ODE_CONFIG_RESULT}" MATCHES "^0$")
-#         # Convert the compile flags to a CMake list.
-#         STRING(REGEX REPLACE " +" ";"
-#           ODE_CONFIG_CFLAGS "${ODE_CONFIG_CFLAGS}")
-#     
-#         # Look for -D options.
-#         SET(ODE_EXTRA_CFLAGS)
-#         FOREACH(flag ${ODE_CONFIG_CFLAGS})
-#           IF("${flag}" MATCHES "^-D")
-#             SET(ODE_EXTRA_CFLAGS ${ODE_EXTRA_CFLAGS} "${flag}")
-#           ENDIF("${flag}" MATCHES "^-D")
-#         ENDFOREACH(flag)
-#       ELSE("${ODE_CONFIG_RESULT}" MATCHES "^0$")
-#         MESSAGE("Error running ${ODE_CONFIG}: [${ODE_CONFIG_RESULT}]")
-#       ENDIF("${ODE_CONFIG_RESULT}" MATCHES "^0$")
-#   ENDIF(ODE_CONFIG)
-  
-    IF (ODE_EXTRA_CFLAGS)
-      SET(ODE_CFLAGS ${ODE_EXTRA_CFLAGS} CACHE STRING "Additional ODE flags")
-      MESSAGE(STATUS "Found additional flags for ODE: ${ODE_CFLAGS}")
-    ELSE (ODE_EXTRA_CFLAGS)
-      SET(ODE_CFLAGS CACHE STRING "Additional ODE flags")
-    ENDIF (ODE_EXTRA_CFLAGS)
+  IF (ODE_EXTRA_CFLAGS)
+    SET(ODE_CFLAGS ${ODE_EXTRA_CFLAGS} CACHE STRING "Additional ODE flags")
+    MESSAGE(STATUS "Found additional flags for ODE: ${ODE_CFLAGS}")
+  ELSE (ODE_EXTRA_CFLAGS)
+    SET(ODE_CFLAGS CACHE STRING "Additional ODE flags")
+  ENDIF (ODE_EXTRA_CFLAGS)
 
   #Try to get header from Release version
   IF(CMAKE_BUILD_TYPE STREQUAL "Release")
@@ -123,6 +79,7 @@ IF (NOT ODE_FOUND)
 
   IF(ODE_INCLUDE_DIR AND ODE_LIBRARY)
      SET(ODE_FOUND TRUE CACHE STRING "Whether ODE was found or not")
+     SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DdDOUBLE")
    ELSE(ODE_INCLUDE_DIR AND ODE_LIBRARY)
      SET(ODE_FOUND FALSE)
      IF(ODE_FIND_REQUIRED)
