91c689ae6880d00bc480015a828c8ce69b0eeb58
Matthieu Zimmer <contact@matthieu-zimmer.net>
2015-05-05 12:02:51 +0200

    increase timeout for memory check

--- scripts/check_utest_memory.bash
+++ scripts/check_utest_memory.bash
@@ -5,13 +5,15 @@ cd $LIB
 
 . locate.bash
 
+set -e
+
 function run_all_test(){
 	goto_root
 	find . -type f -name 'unit-test' | while read atest ; do
 		cd $(dirname $atest)
 		tmp=`mktemp`
-		#15 min timeout
-		timeout 900 valgrind --tool=memcheck --leak-check=full --show-reachable=yes --track-origins=yes --leak-resolution=high ./unit-test >& $tmp
+		#30 min timeout
+		timeout 1800 valgrind --tool=memcheck --leak-check=full --show-reachable=yes --track-origins=yes --leak-resolution=high ./unit-test >& $tmp
 		if [ $? -ne 0 ] ; then
 			cat $tmp
 			rm $tmp
