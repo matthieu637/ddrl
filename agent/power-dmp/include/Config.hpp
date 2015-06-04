55b7d6a6ac2c7a30df79892365ef277789ef9e2d
nicolef <nicolas.lefebvre@inria.fr>
2015-06-04 17:48:16 +0200

    correction var_init : int => float

--- agent/power-dmp/include/Config.hpp
+++ agent/power-dmp/include/Config.hpp
@@ -6,13 +6,13 @@
       unsigned int n_steps_max;
       unsigned int n_motors;
       unsigned int n_sensors;
-      unsigned int var_init;
       unsigned int n_states_per_kernels;
       unsigned int n_kernels_per_dim;
       unsigned int elite;
       unsigned int elite_variance;
       float width_kernel;
       float d_variance;
+      float var_init;
       //std::vector<unsigned int> n_kernels_per_dim;
     };
 #endif // CONFIG_HPP
