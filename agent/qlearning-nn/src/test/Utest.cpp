e1b78a831e0192e4e0c131c720fff6ad7dd534ba
nico <nicolas.lefebvre@inria.fr>
2015-06-12 00:18:17 +0200

    correction bug delete pointeurs

--- agent/qlearning-nn/src/test/Utest.cpp
+++
@@ -1,8 +0,0 @@
-#include "gtest/gtest.h"
-#include <bib/Thread.hpp>
-
-int main(int argc, char **argv) {
-  bib::ThreadTBB::getInstance();
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
