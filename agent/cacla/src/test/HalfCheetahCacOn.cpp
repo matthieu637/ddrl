a78a333ceb3d2775f78f0106c6478d21feee608d
Matthieu Zimmer <contact@matthieu-zimmer.net>
2016-10-07 13:47:30 +0200

    cacla

--- agent/cacla/src/test/HalfCheetahCacOn.cpp
+++ agent/cacla/src/test/HalfCheetahCacOn.cpp
@@ -2,14 +2,14 @@
 #include "arch/Simulator.hpp"
 #include "arch/Example.hpp"
 #include "HalfCheetahEnv.hpp"
-#include "OfflineCaclaAg.hpp"
+#include "BaseCaclaAg.hpp"
 
 int main(int argc, char **argv) {
   FLAGS_minloglevel = 2;
   google::InitGoogleLogging(argv[0]);
   google::InstallFailureSignalHandler();
   
-  arch::Simulator<HalfCheetahEnv, OfflineCaclaAg> s;
+  arch::Simulator<HalfCheetahEnv, BaseCaclaAg> s;
   s.init(argc, argv);
   
   s.run();
