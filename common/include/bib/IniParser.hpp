1798edbebaaadc29e1824c182c513e3c50a26aba
Matthieu Zimmer <contact@matthieu-zimmer.net>
2017-01-11 14:15:10 +0100

    improve parser of ini (higher priority with ':' as separator)

--- common/include/bib/IniParser.hpp
+++ common/include/bib/IniParser.hpp
@@ -10,10 +10,13 @@ namespace bib {
 
 template <typename T>
 std::vector<T>* to_array(const std::string& s) {
+  char separator = ':';
+  if(s.find(separator) == std::string::npos)
+    separator = ',';
   std::vector<T>* result = new std::vector<T>;
   std::stringstream ss(s);
   std::string item;
-  while (std::getline(ss, item, ','))
+  while (std::getline(ss, item, separator))
     result->push_back(boost::lexical_cast<T>(item));
   return result;
 }
