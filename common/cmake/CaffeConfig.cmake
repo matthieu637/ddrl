64f6f6f27437b8fb7327afc1ecae96a62e2f1ac2
Matthieu Zimmer <contact@matthieu-zimmer.net>
2017-01-19 10:57:51 +0100

    build caffe with cmake

--- common/cmake/CaffeConfig.cmake
+++ common/cmake/CaffeConfig.cmake
@@ -1,8 +1,11 @@
-find_package(Boost 1.60 COMPONENTS serialization filesystem system program_options thread REQUIRED)
+find_package(Boost 1.54 COMPONENTS serialization filesystem system program_options thread REQUIRED)
 find_package(Protobuf REQUIRED)
-find_package(Caffe REQUIRED)
 find_package(GLOG)
 find_package(CUDA)
+
+if(${Boost_MAJOR_VERSION} VERSION_EQUAL 1 AND ${Boost_MINOR_VERSION} VERSION_LESS 60)
+       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DBOOST_NO_CXX11_SCOPED_ENUMS")
+endif()
 
 if(NOT DEFINED CUDA_TOOLKIT_ROOT_DIR)
   if(EXISTS "/opt/cuda")
@@ -11,6 +14,26 @@ if(NOT DEFINED CUDA_TOOLKIT_ROOT_DIR)
           set(CUDA_TOOLKIT_ROOT_DIR "/usr/local/cuda-7.0/")
   endif()
   message("CUDA_TOOLKIT_ROOT_DIR not found or specified, set to ${CUDA_TOOLKIT_ROOT_DIR}")
+endif()
+
+find_package(Caffe QUIET)
+if(NOT DEFINED Caffe_INCLUDE_DIRS)
+       configure_file(${CMAKE_SOURCE_DIR}/cmake/Caffe.in caffe-download/CMakeLists.txt)
+       execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+         RESULT_VARIABLE result
+         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/caffe-download )
+
+       if(result)
+         message(FATAL_ERROR "CMake step for caffe failed: ${result}")
+       endif()
+       execute_process(COMMAND ${CMAKE_COMMAND} --build .
+         RESULT_VARIABLE result
+         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/caffe-download )
+       if(result)
+         message(FATAL_ERROR "Build step for caffe failed: ${result}")
+       endif()
+
+       include(${CMAKE_BINARY_DIR}/caffe-build/CaffeConfig.cmake)
 endif()
 
 if(${Caffe_CPU_ONLY})
--- common/cmake/Config.cmake
+++ common/cmake/Config.cmake
@@ -27,4 +27,34 @@ endif()
 set(LIBRARY_OUTPUT_PATH "${CMAKE_SOURCE_DIR}/lib")
 list(APPEND CMAKE_MODULE_PATH "${ROOT_DRL_PATH}/common/cmake/")
 
-include(${ROOT_DRL_PATH}/common/cmake/Callable.cmake)
+include(${ROOT_DRL_PATH}/common/cmake/Callable.cmake 
+
+#-------------------
+# Number of Thread
+#-------------------
+if(NOT DEFINED PROCESSOR_COUNT)
+  # Unknown:
+  set(PROCESSOR_COUNT 0)
+
+  # Linux:
+  set(cpuinfo_file "/proc/cpuinfo")
+  if(EXISTS "${cpuinfo_file}")
+    file(STRINGS "${cpuinfo_file}" procs REGEX "^processor.: [0-9]+$")
+    list(LENGTH procs PROCESSOR_COUNT)
+  endif()
+
+  # Mac:
+  if(APPLE)
+    find_program(cmd_sys_pro "system_profiler")
+    if(cmd_sys_pro)
+      execute_process(COMMAND ${cmd_sys_pro} OUTPUT_VARIABLE info)
+      string(REGEX REPLACE "^.*Total Number Of Cores: ([0-9]+).*$" "\\1"
+        PROCESSOR_COUNT "${info}")
+    endif()
+  endif()
+
+  # Windows:
+  if(WIN32)
+    set(PROCESSOR_COUNT "$ENV{NUMBER_OF_PROCESSORS}")
+  endif()
+endif()
