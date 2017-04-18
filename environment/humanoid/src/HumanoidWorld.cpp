cf53434abb98f5496ea1538e7c224d4402db2287
Matthieu Zimmer <contact@matthieu-zimmer.net>
2017-04-18 13:05:48 +0200

    more flexibility for humanoid

--- environment/humanoid/src/HumanoidWorld.cpp
+++ environment/humanoid/src/HumanoidWorld.cpp
@@ -942,8 +942,8 @@ unsigned int HumanoidWorld::activated_motors() const {
 }
 
 bool HumanoidWorld::final_state() const {
-  //0.9 instead of 1.0 because of soft cfm
-  return internal_state[0] < 0.9 || internal_state[0] > 2.0;
+  //0.8 instead of 1.0 because of soft cfm
+  return internal_state[0] < 0.8 || internal_state[0] > 2.1;
 //   return false;
 }
 
