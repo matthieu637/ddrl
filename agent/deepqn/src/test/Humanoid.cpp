e95b8f6dbf8ecfb649466e24ec5e517eb5b8c3a0
Matthieu Zimmer <contact@matthieu-zimmer.net>
2017-05-03 11:37:55 +0200

    humanoid with ddpg

--- agent/deepqn/src/test/Humanoid.cpp
+++ agent/deepqn/src/test/Humanoid.cpp
@@ -6,7 +6,7 @@
 #include "bib/Assert.hpp"
 #include "arch/Simulator.hpp"
 #include "arch/Example.hpp"
-#include "HalfCheetahEnv.hpp"
+#include "HumanoidEnv.hpp"
 
 // #define POOL_FOR_TESTING
 #include "DeepQNAg.hpp"
@@ -16,7 +16,7 @@ int main(int argc, char **argv) {
   google::InitGoogleLogging(argv[0]);
   google::InstallFailureSignalHandler();
   
-  arch::Simulator<HalfCheetahEnv, DeepQNAg<>> s;
+  arch::Simulator<HumanoidEnv, DeepQNAg<>> s;
   
   s.init(argc, argv);
   
